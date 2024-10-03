710f245e1bfdd7e199f2c2a518dc608534bd8de9
deguchi <deguchi37458@gmail.com>
2024-10-03 16:40:59 +0900

    add username and id to session 🎉

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,29 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { prisma } from "@/lib/prisma";
+import NextAuth, { DefaultSession, NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 type ClientType = {
   clientId: string;
   clientSecret: string;
 };
+declare module "next-auth" {
+  interface User {
+    username: string;
+  }
+  interface Session extends DefaultSession {
+    user: {
+      id: number;
+      username: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: number;
+    username: string;
+  }
+}
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -13,7 +32,21 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     } as ClientType),
   ],
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
+    async session({ session, token }) {
+      const user = await prisma.user.findUnique({
+        where: { email: session?.user?.email || "" },
+      });
+
+      if (user) {
+        session.user.id = user.id;
+        session.user.username = user.username;
+      }
+      return session;
+    },
     async redirect() {
       return `/welcome`;
     },
